57504999b2ae22c8e116a0460aadca51e511239e
XDRosenheim <patrickrosenheim@gmail.com>
2016-01-19 14:52:53 +0100

    A quick start.
    More to come. I hope.

---
+++ Chatting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chatting {
+    internal class Program {
+        private static void Main( string[] args ) {
+            var exit = false;
+            var readOpt = Console.Read();
+            while(!exit) {
+                switch(readOpt) {
+                    case 1:
+                        Chatter.Connect();
+                        exit = true;
+                        break;
+                    case 2:
+                        var port = Console.ReadLine();
+                        Chatter.Server();
+                        exit = true;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Chatting/chatter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chatting {
+    internal class Chatter {
+        public static void Connect() {
+            string readIp, nickname;
+            var connectPort = 0;
+            IPAddress connectIp = null;
+            var isIp = false;
+            var isPort = false;
+            var isNick = false;
+            //
+            // Nickname / Screenname / Username
+            //
+            while(!isNick) {
+                try {
+                    Console.Write( "Enter a nickname: " );
+                    nickname = Console.ReadLine();
+                    isNick = true;
+                } catch(Exception) {
+                    // Exception!
+                }
+            }
+            //
+            // IP Address
+            //
+            while(!isIp) {
+                try {
+                    Console.Write( "IP: \t" );
+                    readIp = Console.ReadLine();
+                    if(readIp != null) {
+                        connectIp = IPAddress.Parse( readIp );
+                    }
+                    isIp = true;
+                } catch(Exception) {
+                    // Exception!
+                    Console.Clear();
+                }
+            }
+            //
+            // Port
+            //
+            while(!isPort) {
+                try {
+                    Console.Write( "Port:\t" );
+                    var readPort = Console.ReadLine();
+                    connectPort = int.Parse( readPort );
+                    isPort = true;
+                } catch(Exception) {
+                    // Exception!
+                    Console.SetCursorPosition( 0, 2 );
+                    Console.Write( "\r" + new string( ' ', Console.WindowWidth ) );
+                }
+            }
+            if(connectIp != null) {
+                var client = new TcpClient( connectIp.ToString(), connectPort );
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Server() {
+            var listener = TcpListener.Create(1337);
+        }
+        public static void Server(int port) {
+            var listener = TcpListener.Create(port);
+        }
+    }
+}
